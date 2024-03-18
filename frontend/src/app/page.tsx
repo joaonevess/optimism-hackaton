2162792fde3f5c41d2c4c8832f7432b544d51a1e
Gabriel <gabrieltollini@hotmail.com>
2024-03-18 19:50:31 -0300

    more element filtering

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="main-div flex flex-col min-h-dvh justify-between ">
+    <div className="main-div flex flex-col min-h-dvh items-center justify-between ">
       <header className="bg-[#ff0000] main-div-auth-child">
         placeholder header  
       </header>
