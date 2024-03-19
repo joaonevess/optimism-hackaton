3bc7cc57e8645bf037d84407df6844cc99f3ecbf
joaonevess <joaocns0@protonmail.com>
2024-03-19 03:11:47 -0300

    sibyl refactor with response storage

--- sibyl/src/Sibyl.sol
+++ sibyl/src/Sibyl.sol
@@ -5,20 +5,27 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
 
 contract Sibyl is AccessControl, Pausable {
-    event QueryRequest(uint256 requestId, Query query);
-    event QueryResponse(uint256 requestId, Response response);
+    event QueryRequested(
+        uint256 requestId,
+        string question,
+        AIModel model,
+        ResponseType responseType
+    );
+    event QueryCompleted(uint256 requestId);
 
     struct Query {
+        uint256 requestId;
         string question;
-        ResponseType responseType;
+        RequestStatus status;
         AIModel model;
+        Response response;
+        ResponseType responseType;
     }
 
-    enum ResponseType {
-        Bool,
-        Int,
-        String,
-        Error
+    enum RequestStatus {
+        Pending,
+        Responded,
+        Failed
     }
 
     enum AIModel {
@@ -28,63 +35,76 @@ contract Sibyl is AccessControl, Pausable {
         Claude
     }
 
-    enum RequestStatus {
-        Pending,
-        Responded,
-        Failed
+    enum ResponseType {
+        Bool,
+        Int,
+        String
     }
 
     struct Response {
-        ResponseType responseType;
         uint256 integerResponse;
         string stringResponse;
         bool boolResponse;
     }
 
     uint256 private requestCounter;
-    mapping(uint256 => RequestStatus) private requestStatus;
+    mapping(uint256 => Query) private requests;
 
     // Public functionality
-    function query(Query calldata queryData) public returns (uint256) {
+    function query(
+        string memory question,
+        AIModel model,
+        ResponseType responseType
+    ) public whenNotPaused returns (uint256) {
+        require(bytes(question).length > 0, "Sibyl: Question cannot be empty");
+
         uint256 requestId = requestCounter++;
-        requestStatus[requestId] = RequestStatus.Pending;
-        emit QueryRequest(requestId, queryData);
+        requests[requestId] = Query(
+            requestId,
+            question,
+            RequestStatus.Pending,
+            model,
+            Response(0, "", false),
+            responseType
+        );
+        emit QueryRequested(requestId, question, model, responseType);
         return requestId;
     }
 
     // Data provider functionality
-    function respond(
+    function fulfillRequest(
         uint256 requestId,
         Response calldata response
-    ) public onlyRole(DATA_PROVIDER_ROLE) {
+    ) public onlyRole(DATA_PROVIDER_ROLE) whenNotPaused {
         require(
-            requestStatus[requestId] == RequestStatus.Pending,
-            "Request is not pending"
+            requests[requestId].status == RequestStatus.Pending,
+            "Sibyl: Request not in Pending state"
         );
 
-        requestStatus[requestId] = RequestStatus.Responded;
-        emit QueryResponse(requestId, response);
+        requests[requestId].response = response;
+        requests[requestId].status = RequestStatus.Responded;
+        emit QueryCompleted(requestId);
     }
 
     function markRequestAsFailed(
         uint256 requestId
-    ) public onlyRole(DATA_PROVIDER_ROLE) {
+    ) public onlyRole(DATA_PROVIDER_ROLE) whenNotPaused {
         require(
-            requestStatus[requestId] == RequestStatus.Pending,
+            requests[requestId].status == RequestStatus.Pending,
             "Request is not pending"
         );
 
-        requestStatus[requestId] = RequestStatus.Failed;
-        emit QueryResponse(
-            requestId,
-            Response(ResponseType.Error, 0, "", false)
-        );
+        requests[requestId].status = RequestStatus.Failed;
+
+        emit QueryCompleted(requestId);
     }
 
     // Admin functionality
     constructor(address defaultAdmin) {
         _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
     }
+    event DataProviderRegistered(address provider);
+    event DataProviderRemoved(address provider);
 
     bytes32 public constant DATA_PROVIDER_ROLE =
         keccak256("DATA_PROVIDER_ROLE");
@@ -92,13 +112,25 @@ contract Sibyl is AccessControl, Pausable {
     function registerDataProvider(
         address _newProvider
     ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(
+            !hasRole(DATA_PROVIDER_ROLE, _newProvider),
+            "Sibyl: Address is already a provider"
+        );
+
         _grantRole(DATA_PROVIDER_ROLE, _newProvider);
+        emit DataProviderRegistered(_newProvider);
     }
 
     function removeDataProvider(
         address _provider
     ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(
+            hasRole(DATA_PROVIDER_ROLE, _provider),
+            "Sibyl: Address is not a provider"
+        );
+
         _revokeRole(DATA_PROVIDER_ROLE, _provider);
+        emit DataProviderRemoved(_provider);
     }
 
     function pause() public onlyRole(DEFAULT_ADMIN_ROLE) {
