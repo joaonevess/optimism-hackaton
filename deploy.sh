b44dcab5cdcd6458c4dc734fb6aae31a49f8d47d
Gabriel <gabrieltollini@hotmail.com>
2024-03-20 20:00:13 -0300

    deploy fix

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
+(touch frontend/src/lib/SibylDeployment.json);
 (cd sibyl && forge create --rpc-url http://127.0.0.1:8545  --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 src/Sibyl.sol:Sibyl --json > ../frontend/src/lib/SibylDeployment.json);
