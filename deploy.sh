e96826f6126bdcde921972b171771da0fdf0e271
joaonevess <joaocns0@protonmail.com>
2024-03-22 23:40:46 -0300

    variable renaming

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # Default Anvil private key/address for local development
-OWNER_ADDRESS="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+DEFAULT_ANVIL_ADDRESS="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+DEFAULT_ANVIL_PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 
-cd sibyl && forge create --chain optimism  --rpc-url http://127.0.0.1:8545  --constructor-args  $OWNER_ADDRESS --private-key $PRIVATE_KEY src/Sibyl.sol:Sibyl --json | tee ../frontend/src/lib/SibylDeployment.json ../backend/abi/SibylDeployment.json
+cd sibyl && forge create --chain optimism  --rpc-url http://127.0.0.1:8545  --constructor-args  $DEFAULT_ANVIL_ADDRESS --private-key $DEFAULT_ANVIL_PRIVATE_KEY src/Sibyl.sol:Sibyl --json | tee ../frontend/src/lib/SibylDeployment.json ../backend/abi/SibylDeployment.json
 
 
