f1bc619056ce13f6552ca8376f3891415446b7fc
joaonevess <joaocns0@protonmail.com>
2024-03-22 23:38:54 -0300

    small fixes

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
-(touch frontend/src/lib/SibylDeployment.json);
-(cd sibyl && forge create --rpc-url http://127.0.0.1:8545  --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 src/Sibyl.sol:Sibyl --json > ../frontend/src/lib/SibylDeployment.json);
-(cp frontend/src/lib/SibylDeployment.json backend/abi/SibylDeployment.json);
+# Default Anvil private key/address for local development
+OWNER_ADDRESS="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+PRIVATE_KEY="0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+
+cd sibyl && forge create --chain optimism  --rpc-url http://127.0.0.1:8545  --constructor-args  $OWNER_ADDRESS --private-key $PRIVATE_KEY src/Sibyl.sol:Sibyl --json | tee ../frontend/src/lib/SibylDeployment.json ../backend/abi/SibylDeployment.json
+
+
